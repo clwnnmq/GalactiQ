db3a0a7ce1312d53d5bd5e6b4c5cee8d9ea2378b
clwnnmq <forapiwork@gmail.com>
2024-08-30 23:12:48 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,28 @@
+
+import CardPage from "./assets/CardPage"
+import FAQsPage from "./assets/FAQsPage"
+import FooterPage from "./assets/FooterPage"
+import HomePage from "./assets/HomePage"
+import ReviewsPage from "./assets/ReviewsPage"
+import ShopPage from "./assets/ShopPage"
+import TeamPage from "./assets/TeamPage"
+import WelcomePage from "./assets/WelcomePage"
+// import NavBar from "./NavBar"
+
+function App() {
+  return (
+    <>
+      {/* <NavBar /> */}
+     <HomePage />
+     <WelcomePage />
+     <CardPage />
+     <ShopPage />
+     <TeamPage />
+     <ReviewsPage />
+     <FAQsPage />
+     <FooterPage />
+    </>
+  )
+}
+
+export default App
